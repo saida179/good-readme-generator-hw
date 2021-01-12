53d99f1fd649d75847c09cfabed6e93b3d1f274c
saida179 <69127945+saida179@users.noreply.github.com>
2021-01-12 01:23:38 -0700

    starting over

--- generateMarkdown.js
+++ generateMarkdown.js
@@ -1,18 +1,1 @@
-// function to generate markdown for README
-function generateMarkdown(data) {
-  return `# ${data.title}
 
-`;
-}
-
-function homeWorkTitle(title){
-  let generate = "# homework title: ${title} ";
-  return generate
-}
-
-function homeWorkDescription(Description){
-  let generate =" # homework Description: ${Description} ";
-  return generate
-}
-
-module.exports = generateMarkdown;
